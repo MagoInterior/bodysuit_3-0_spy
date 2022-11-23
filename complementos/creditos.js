4f40c705035fb13b2d07ab322edf4d482e84ead5
Álex García <106597174+Herrera1234@users.noreply.github.com>
2022-11-22 20:45:15 -0500

    Add files via upload

---
+++ complementos/creditos.js
@@ -0,0 +1,21 @@
+
+let handler = async m => m.reply(`
+┏━━━━━ ★彡[ᴄʀᴇᴅɪᴛᴏꜱ]彡★ ━━━━━ ◈
+┃࿊⃟♦- Creado por :*alex™✓*
+┃ Contacto: wa.me/573245104054
+┃ Repositorio del bot: 
+┃ https://github.com/OdinTm/INFINIXBOT-MD 
+┃────────➢
+┃࿊⃟♦- Base: *Nurutomo y Bochilgaming*
+┃github:https://github.com/BochilGaming/games-wabot
+┃────────➢
+┃࿊⃟♦- *Grupo oficial del bot*, 
+┃https://chat.whatsapp.com/IlmNa0htasZDNGaVriSrCQ
+┃────────➢ 
+┗━━━━◈❮📢🪄🪀❯◈━━━━━ ◈
+
+`.trim()) 
+handler.help = ['ᴄʀᴇᴅɪᴛᴏꜱ']
+handler.tags = ['main']
+handler.command = /^(creditos|créditos|creadores)$/i
+export default handler
